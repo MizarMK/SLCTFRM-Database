43f219aa61ccee656118c24421bd94fa70202f90
Brandon Valiente <brandonvaliente7@gmail.com>
2021-12-03 11:07:19 -0600

    Fixed roster.py to work now

--- SLCTFRM/roster.py
+++ SLCTFRM/roster.py
@@ -1,6 +1,5 @@
 import pymysql
 import csi3335fall2021 as cfg
-import sys
 import warnings
 from datetime import date
 
@@ -12,60 +11,50 @@ def calculateAge(birthDate):
     return age
 
 
-# Checks for the correct amount of arguments
-params = []
-argc = len(sys.argv)
-if argc != 2:
-    sys.stderr.write("Error in argument count!")
-    exit(1)
-else:
-    params = sys.argv[1]
-
 # Connects to the database
 con = pymysql.connect(host=cfg.mysql['host'], user=cfg.mysql['username'], password=cfg.mysql['password'],
                       database=cfg.mysql['database'])
 warnings.simplefilter("ignore")
 
+# HERE IS WHERE YOU PUT THE TEAMID AND YEARID FOR THE ROSTER YOU WANT
+params = ["HOU", "2020"]
+
 # Sets up the SQL query to send to database
 try:
-    year = 2020
-    playerName = ""
-    playerAge = 0
-    birthPlace = None
-    hitter = True
-    pitcher = False
-
     cur = con.cursor()
-    sql = '''SELECT p.playerID, ppl.nameFull as 'Name', ppl.birthCountry, ppl.birthYear,
+    sql = '''SELECT p.playerID, concat(ppl.nameFirst, ' ', ppl.nameLast) as 'Name', ppl.birthCountry, ppl.birthYear,
      ppl.birthMonth, ppl.birthDay
-     FROM pitching p JOIN batting b ON (b.yearID = p.yearID AND b.playerID = p.playerID)
-     JOIN people ppl ON (ppl.personID = p.playerID)
-     WHERE p.yearID = 2020 AND p.teamID = %s'''
+     FROM appearances p JOIN people ppl ON (ppl.personID = p.playerID)
+     WHERE p.teamID = %s AND p.yearID = %s'''
     cur.execute(sql, params)
 
     # Gets the results from the query and prints them out
-    finalResults = []
+    finalResults = dict()
     results = cur.fetchall()
     for row in results:
-        finalResults.append([row[0], row[1], row[2], calculateAge(date(row[3], row[4], row[5])), "N", "N"])
+        finalResults[row[0]] = ([row[1], row[2], calculateAge(date(row[3], row[4], row[5])), "N", "N"])
 
-    # [playerID, name, birth, age, pitcher, batter]
-    for row in finalResults:
-        params = row[0]
-        sql = '''SELECT playerID FROM appearances WHERE G_batting > 0 AND playerID = %s'''
+    # Key: playerID, Value: [name, birth, age, pitcher, batter]
+    # This sorts it into last name order
+    for row in dict(sorted(finalResults.items(), key=lambda item: item[1])):
+        params[0] = row
+        sql = '''SELECT playerID FROM appearances WHERE G_batting > 0 AND playerID = %s AND yearID = %s'''
         cur.execute(sql, params)
         results = cur.fetchall()
         if len(results) > 0:
-            row[5] = "Y"
-        sql = '''SELECT playerID FROM appearances WHERE G_pitcher > 0 AND playerID = %s'''
+            temp = finalResults[row]
+            temp[4] = "Y"
+            finalResults[row] = temp
+        sql = '''SELECT playerID FROM appearances WHERE G_pitcher > 0 AND playerID = %s AND yearID = %s'''
         cur.execute(sql, params)
         results = cur.fetchall()
         if len(results) > 0:
-            row[4] = "Y"
+            temp = finalResults[row]
+            temp[3] = "Y"
+            finalResults[row] = temp
 
     for r in finalResults:
-        r.pop(0)
-        for c in r:
+        for c in finalResults[r]:
             print(c, end=", ")
         print()
 
